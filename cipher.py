97fbc784f0a23c11a989ced359db91987c830589
Alberto Mejia <albertomejia295@gmail.com>
2019-12-30 02:05:48 -0500

    Add blank project files

---
+++ cipher.py
@@ -0,0 +1,12 @@
+class Cipher:
+    def __init__(self, codestring):
+    	# Add your code here
+    	pass
+        
+    def encode(self, plaintext):
+    	# Add your code here
+        pass
+
+    def decode(self, ciphertext):
+    	# Add your code here
+    	pass
